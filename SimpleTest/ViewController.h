9b75c5e91f97b7e54cd2a2dca5e6783e2be1be9f
Alex Um <alexum@Alexs-MacBook-Pro.local>
2015-07-21 10:13:14 +0900

    Initial Commit

---
+++ SimpleTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTest
+//
+//  Created by Alex Um on 7/21/15.
+//  Copyright (c) 2015 Streamlyzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleTest
+//
+//  Created by Alex Um on 7/21/15.
+//  Copyright (c) 2015 Streamlyzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
